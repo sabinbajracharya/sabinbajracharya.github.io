058eb064264b3badfe0dfb68bedb6b9be5bf9505
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 04:05:49 +0545

    Add reference link that fixes firebase database integration with netlify

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,3 +1,6 @@
+// Fix: Lambda timeout and bunch of warnings when using firebase database
+// https://github.com/netlify/netlify-lambda/issues/112#issuecomment-488644361
+
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
