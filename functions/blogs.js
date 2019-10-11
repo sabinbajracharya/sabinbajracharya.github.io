a25dda0efdddf5340ede19110074a236091e8649
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 04:06:36 +0545

    Query firebase firestore db to return data in json format

--- functions/blogs.js
+++ functions/blogs.js
@@ -1,6 +1,25 @@
+const admin = require('firebase-admin');
+let serviceAccount = require('../serviceAccountKey.json');
+
+const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://peronal-blog.firebaseio.com",
+})
+
+let db = admin.firestore();
+
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World" })
-    };
+    let citiesRef = db.collection('blog');
+    let snapshot = await citiesRef.get();
+    if (snapshot.empty) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "No blog posts found." })
+        };
+    } else {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ data: snapshot.docs.map( doc => doc.data()) })
+        };
+    }
 };
