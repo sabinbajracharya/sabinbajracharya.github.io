8b2aa5c7a2feb901d609a5e4cf267e04eb92e76e
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 16:27:39 +0545

    Rquired serviceAccount object from init.js

--- functions/blogs.js
+++ functions/blogs.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-let serviceAccount = require('../serviceAccountKey.json');
+let serviceAccount = require('./config/init.js')();
 
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
