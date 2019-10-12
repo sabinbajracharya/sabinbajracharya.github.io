76bec5607b09136eb6e5eae6f94a36370cc41662
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 19:11:08 +0545

    Debugging: Initialize firebase admin

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,20 @@
 const admin = require('firebase-admin');
 let serviceAccount = require('./config/init.js')();
 
+let app = null;
+
+try {
+    app = admin.app();
+} catch (error) {
+    app = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIRESTORE_DB_URL,
+
+    });
+}
+
+let db = admin.firestore();
+
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
