14c8a20293b8509a186d555e6d46f46334c5153c
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 17:56:02 +0545

    Fix: Netlify lambda TypeError

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,8 +1,16 @@
 // Fix: Lambda timeout and bunch of warnings when using firebase database
 // https://github.com/netlify/netlify-lambda/issues/112#issuecomment-488644361
 
+/* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
+
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({ "global.GENTLY": false })
+  ],
+  node: {
+    __dirname: true,
+  },
   externals: [nodeExternals()],
 };
