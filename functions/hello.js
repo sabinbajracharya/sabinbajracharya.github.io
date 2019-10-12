238c857972d6486d680eac1186b0bd0694563cd5
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-12 19:42:31 +0545

    Remove db query from hello endpoint

--- functions/hello.js
+++ functions/hello.js
@@ -16,17 +16,8 @@ try {
 let db = admin.firestore();
 
 exports.handler = async (event, context) => {
-    let citiesRef = db.collection('blog');
-    let snapshot = await citiesRef.get();
-    if (snapshot.empty) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "No blog posts found." })
-        };
-    } else {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ data: snapshot.docs.map( doc => doc.data()) })
-        };
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "No Hello World." })
+    };
 };
