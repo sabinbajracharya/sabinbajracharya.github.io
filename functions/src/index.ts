3125ad0a1068895393b5db8a1e41364ff47de61c
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-13 17:46:05 +0545

    Use expressjs with firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,34 @@
+import * as express from 'express';
+import * as cors from 'cors';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+const app = express();
+app.use(cors({ origin: true }));
+
 admin.initializeApp();
 
-export const blogs = functions.https.onRequest(async (request, response) => {
+app.get('/', async (req, res) => {
+    try {
+        res.send({message: "It Works!"})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.get('/blogs', async (req, res) => {
     try {
         const db = admin.firestore()
         const blogsRef = db.collection('blog')
         const snapshot = await blogsRef.get()
         const result = snapshot.docs.map( doc => doc.data())
 
-        response.send(result)
+        res.send(result)
     } catch (error) {
-        response.status(500).send(error)
+        res.status(500).send(error)
     }
-});
+})
+
+exports.api = functions.https.onRequest(app)
+
+// https://firebase.google.com/docs/functions/http-events
