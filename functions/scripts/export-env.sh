dd2148aced1c824148a2f8d23a53fe5b71dd5ead
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-10-13 17:03:27 +0545

    Script to export firebase config and credential

---
+++ functions/scripts/export-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export FIREBASE_CONFIG="firebase-config.json"
+export GOOGLE_APPLICATION_CREDENTIALS="firebase-admin-credential.json"
+
+echo "Exported: FIREBASE_CONFIG"
+echo "Exported: GOOGLE_APPLICATION_CREDENTIALS"
